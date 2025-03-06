a92b88dc3ee2a733f89007ac7ee43a7117a62f00
Joshua Niam <joshuaniamjy@gmail.com>
2025-03-06 18:14:51 +0800

    Tested Tree initialize() function (previously add()) and completed write() function, with test finish

--- tree.py
+++ tree.py
@@ -98,60 +98,93 @@ class Tree:
 
     branches = {"math": math_twigs, "science": science_twigs}
 
-    def add(self):
+    def intiialize(self):
         '''Creates a paper'''
-        print(key for key in Tree.branches.keys())
+        print(list(Tree.branches.keys()))
         c1 = intinput("What type of paper are you publishing/adding (1: math, 2: science)?"
                       ":", 2)
-        if c1 == 1:
+
+        if c1 == "1":
             c1 = "math"
             print("\n")
-            for index, key in enumerate(Tree.math_twigs.keys()):
+            for index, key in enumerate(Tree.math_twigs.keys(), start=1):
                 print(f"{index}. {key}")
             c2 = intinput("What type of paper are you publishing (enter index)?",
                           len(Tree.math_twigs.keys()))
-            c2 = Tree.math_twigs[c2]
-            for index, key in enumerate(c2):
+            for k, x in enumerate(Tree.math_twigs.keys(), start=1):
+                if k == int(c2):
+                    c2name = x
+                    break
+            for index, value in enumerate(Tree.math_twigs.values(), start=1):
+                if index == int(c2):
+                    c2 = value
+                    break
+            for index, key in enumerate(c2,start=1):
                 print(f"{index}. {key}")
+            #c stands for choice
             c3 = intinput("What type of paper are you publishing (enter index)?",
                           len(c2.keys()))
             for x, y in enumerate(c2.keys()):
-                if x == c3:
+                if x == int(c3):
                     c3name = y
+                    break
+            for index, key in enumerate(c2.keys(), start=1):
+                if index == int(c3):
+                    c3 = key
+                    break
 
-            for k, x in Tree.math_twigs.items():
-                if x == c2:
-                    c2name = k
 
             title = input("What is the title of the paper you are publishing/adding?: ")
             author = input("What is the author of the paper you are publishing/adding?: ")
             date = input("When was the paper completed? (DD/MM/YYYY): ")
             c2[c3].append(Paper(author, title, date, c1, c2name, c3name))
+            print("Paper has been successfully published/initialized.")
 
 
-        elif c1 == 2:
+        elif c1 == "2":
             c1 = "science"
             print("\n")
-            for index, key in enumerate(Tree.science_twigs.keys()):
+            for index, key in enumerate(Tree.science_twigs.keys(), start=1):
                 print(f"{index}. {key}")
             c2 = intinput("What type of paper are you publishing (enter index)?",
-                          len(Tree.science_twigs.keys()))
-            c2 = Tree.science_twigs[c2]
-            for index, key in enumerate(c2):
+                          len(Tree.math_twigs.keys()))
+            for k, x in enumerate(Tree.science_twigs.keys(), start=1):
+                if k == int(c2):
+                    c2name = x
+                    break
+            for index, value in enumerate(Tree.science_twigs.values(), start=1):
+                if index == int(c2):
+                    c2 = value
+                    break
+            for index, key in enumerate(c2, start=1):
                 print(f"{index}. {key}")
             c3 = intinput("What type of paper are you publishing (enter index)?",
                           len(c2.keys()))
-            for x, y in enumerate(c2.keys()):
-                if x == c3:
+            for x, y in enumerate(c2.keys(), start=1):
+                if x == int(c3):
                     c3name = y
-
-            for k, x in Tree.science_twigs.items():
-                if x == c2:
-                    c2name = k
+                    break
+            for index, key in enumerate(c2.keys(), start=1):
+                if index == int(c3):
+                    c3 = key
+                    break
 
             title = input("What is the title of the paper you are publishing/adding?: ")
             author = input("What is the author of the paper you are publishing/adding?: ")
             date = input("When was the paper completed? (DD/MM/YYYY): ")
             c2[c3].append(Paper(author, title, date, c1, c2name, c3name))
+            print("Paper has been successfully published/initialized.")
 
+    def write(self, paper : Paper):
+        path = input(r"Enter file path (e.g. 'Papers\relativity.txt): ")
+        if not paper.extract(path):
+            return False
+        else:
+            return True
+
+if __name__ == "__main__":
+    paper1 = Paper("Joshua", "JoshuaNIam", "11/2/2025", "math", "Algebra"
+                   , "Elementary algebra")
+    t = Tree()
+    t.write(paper1)
 
